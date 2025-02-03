ad284e726a3d64e976abf8528099e2f5f76716a7
shivrajr1 <156070949+shivrajr1@users.noreply.github.com>
2025-02-03 12:45:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -48,10 +48,8 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
   cookie:{
-    // expires: new Date(Date.now() + (30 * 24 * 3600 * 1000)),
     maxAge:7*24*3600*1000,
     httpOnly:true,
-    sameSite: 'none'
   }
 }))
 
